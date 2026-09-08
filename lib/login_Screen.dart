81cdb4dbe3cde8a9f1fb2711814e1397c23bd2bb
Sarmad Farid <sarmadfarid0@gmail.com>
2026-09-08 16:23:50 +0500

    improve login screen design and add login method and impliment the concept of setstate

--- lib/login_Screen.dart
+++ lib/login_Screen.dart
@@ -1,71 +1,102 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  static const String savedEmail = "aqsaydost@gmail.com";
+  static const String savedpass = "aqsa@786";
+  String message = "";
+
+  void login() {
+    print("email: '${emailController.text}'");
+    print("pass: '${passwordController.text}'");
+
+    if (emailController.text.isEmpty) {
+      setState(() {
+        message = "email required!";
+      });
+      return;
+    }
+
+    if (passwordController.text.isEmpty) {
+      setState(() {
+        message = "password required!";
+      });
+      return;
+    }
+
+    final email = emailController.text.trim();
+    final password = passwordController.text.trim();
+
+    if (email == savedEmail && password == savedpass) {
+      setState(() {
+        message = "Login successfully";
+      });
+    } else {
+      setState(() {
+        message = "invalid credentials";
+      });
+    }1
+
+    print("entered email: '$email'");
+    print("saved email: '$savedEmail'");
+    print("entered pass: '$password'");
+    print("saved pass: '$savedpass'");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-  backgroundColor: const Color.fromARGB(255, 230, 159, 134),
-        appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 230, 159, 134),
-      ),
+      backgroundColor: const Color.fromARGB(255, 230, 159, 134),
 
       body: Column(
         children: [
-          Expanded(
-           
-            child: Container(
-              color: const Color.fromARGB(255, 230, 159, 134),
+          SizedBox(height: 35),
+          Text(message),
 
-              child: Center(
-                child: Column(
-                  children: [
-                    Image.asset(
-                      "cook.png",
-                      width: 200,
-                    ),
-
-                    Text(
-                      "Home cook maid",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w100,
-                        color: const Color.fromARGB(255, 51, 14, 2),
-                      ),
-                    ),
-
-                    Text(
-                      "Feel free to cook and treat yourself",
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 51, 14, 2),
-                        fontSize: 17,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+          Image.asset("cook.png", width: 200, height: 200, fit: BoxFit.cover),
+          Text(
+            "Home cook maid",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w100,
+              color: const Color.fromARGB(255, 51, 14, 2),
             ),
           ),
 
+          Text(
+            "Feel free to cook and treat yourself",
+            style: TextStyle(
+              color: const Color.fromARGB(255, 51, 14, 2),
+              fontSize: 17,
+            ),
+          ),
+
+          SizedBox(height: 30),
+
           Expanded(
-        
             child: Container(
+              padding: EdgeInsets.all(20),
               width: double.infinity,
-  decoration: BoxDecoration(
-    color: const Color.fromARGB(255, 223, 211, 211),
-    borderRadius: BorderRadius.only(
-      topLeft: Radius.circular(30),
-      topRight: Radius.circular(30),
-    ),
-  ),
-              
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 223, 211, 211),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
 
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-
-                  child: Column(
+              child: ListView(
+                children: [
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
@@ -79,120 +110,119 @@ class LoginScreen extends StatelessWidget {
 
                       Text(
                         "Login to continue using our platform",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.blueGrey,
-                        ),
+                        style: TextStyle(fontSize: 15, color: Colors.blueGrey),
                       ),
 
-                      SizedBox(height: 20),
+                      SizedBox(height: 25),
 
                       Text(
                         "Email",
                         style: TextStyle(
                           fontSize: 20,
                           color: Colors.brown,
-                          fontWeight: FontWeight.w500,
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
 
                       TextField(
+                        controller: emailController,
                         decoration: InputDecoration(
                           hintText: "Enter your mail",
                         ),
                       ),
 
-                      SizedBox(height: 20),
+                      SizedBox(height: 25),
 
                       Text(
                         "Password",
                         style: TextStyle(
                           fontSize: 20,
                           color: Colors.brown,
-                          fontWeight: FontWeight.w500,
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
 
                       TextField(
-                        
+                        controller: passwordController,
                         decoration: InputDecoration(
-                        
-                         
                           hintText: "Enter your Password",
-                         
-                        suffixIcon: Icon(Icons.remove_red_eye),
+                          suffixIcon: Icon(Icons.remove_red_eye),
                         ),
                       ),
-                   Align(
-                    alignment: Alignment.centerRight,
-                   child:  Text("Forgot your password?",style: 
-                   TextStyle(
-                    color: Colors.brown,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w200,
 
-
-                   ),),
-                   ),
-                    SizedBox(height: 20,),
-                 ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                  
-                    backgroundColor: const Color.fromARGB(255, 63, 37, 28),
-                     minimumSize: Size(10000, 50),
-                  
-                  ),
-                 onPressed: (){
-
-                 }, child: Text("Login",
-                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                 ),
-                 
-                 
-                 
-                 ),),
-                 SizedBox(height: 20,),
-                  ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                  
-                    backgroundColor: const Color.fromARGB(255, 236, 199, 187),
-                     minimumSize: Size(10000, 50),
-                  
-                  ),
-                 onPressed: (){
-
-                 }, child: Text("Login with Google", 
-                 style: TextStyle(
-                  color: const Color.fromARGB(255, 54, 53, 53),
-                  fontSize: 18,
-                 ),
-                 
-                 
-                 
-                 ),),
-                 SizedBox(height: 20,),
-                 Center(
-                  
-                   child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                   Text("Don't Have account?"),
-                     SizedBox(width: 6,),
-                    Text("Sign up",style:TextStyle(
-                      color: const Color.fromARGB(255, 80, 26, 6),
-                      fontSize: 17,
-                      fontWeight: FontWeight.w300,
-                    ) ,),
-                     ],
-                    ),
-                 ),
-                   
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          "Forgot your password?",
+                          style: TextStyle(
+                            color: Colors.brown,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w200,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            63,
+                            37,
+                            28,
+                          ),
+                          minimumSize: Size(10000, 50),
+                        ),
+                        onPressed: () {
+                          login();
+                          print(message);
+                        },
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                      SizedBox(height: 25),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            236,
+                            199,
+                            187,
+                          ),
+                          minimumSize: Size(10000, 50),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Login with Google",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 54, 53, 53),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Center(
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: .center,
+                          children: [
+                            Text("Don't Have account?"),
+                            SizedBox(width: 6),
+                            Text(
+                              "Sign up",
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 80, 26, 6),
+                                fontSize: 17,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
-                   
                   ),
-                ),
+                ],
               ),
             ),
           ),
