1b154aae5f7241c5761516da369fc173fcce46db
Aqsayy <aqsay379@gmail.com>
2026-09-17 07:18:32 -0700

    all task practice

---
+++ lib/checktasks/list.dart
@@ -0,0 +1,15 @@
+// 1 Create a list of five numbers.
+void main(){
+List<int> numbers = [1, 3, 4, 5, 7];
+
+// 2 Add two more numbers to the list.
+numbers.add(2);
+numbers.add(8);
+numbers.remove(23);
+
+
+print(numbers.first);
+print(numbers.last);
+ (numbers.sort);
+ print(numbers);
+}
